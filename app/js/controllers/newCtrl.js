ae9a84a7ca9fd16f16635580ce4261fcb1e21bf8
Michael Boyle <michael.jarvis.boyle@gmail.com>
2017-05-21 11:13:00 -0400

    Update frontend to work with new backend

--- app/js/controllers/newCtrl.js
+++ app/js/controllers/newCtrl.js
@@ -1,17 +1,22 @@
 
-app.controller('NewCtrl', ['$scope', '$rootScope', '$location', '$http', 'Records',
-               function($scope, $rootScope, $location, $http, Records) {
+app.controller('NewCtrl', ['$scope', '$rootScope', '$location', '$http',
+               '$window', 'Records', 
+               function($scope, $rootScope, $location, $http,
+                        $window, Records) {
 
   $scope.categories = ['Medical', 'Miscellaneous', 'Sports'];
 
   $scope.shapes = ['square', 'circle', 'person', 'person_rect'];
 
+  $scope.types = ['png', 'svg'];
+
+  // Additional optional params defined in the view
   $scope.imgParams = {
     'shape': 'person_rect',
     'color1': 'red',
     'color2': 'black',
-    'index': 0.75,
-    'right': false
+    'index': 0.0,
+    'type': 'svg'
   };
 
   $scope.save = false;
@@ -21,31 +26,25 @@ app.controller('NewCtrl', ['$scope', '$rootScope', '$location', '$http', 'Record
   function getImage() {
     var params = [];
     for (var param in $scope.imgParams) {
-      params.push(param + '=' + $scope.imgParams[param]);
+      if ($scope.imgParams[param] !== null) {
+        params.push(param + '=' + $scope.imgParams[param]);
+      }
     }
     params.push('odds=' + $scope.record.odds);
     var paramString = params.join('&');
     $rootScope.imgSrc = '/api/draw?' + paramString;
-    $location.path('/image');
-    /*
-    $http({
-      method: 'GET',
-      url: '/api/draw?' + paramString
-    }).then(function successCallback(response) {
-      $rootScope.imgSrc = response.data;
-      $location.path('/image');
-    }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    }); */
+    $window.location.href = 'http://odds-view.appspot.com' + $rootScope.imgSrc;
   }
 
+  $scope.loading = false;
+
   $scope.submit = function() {
+    $scope.loading = true;
     // Deactivate button and animate to page where image is shown
     // Show image loading icon
     if ($scope.save) {
       $scope.record.$save();
     }
     getImage();
-  }
+  };
 }]);
